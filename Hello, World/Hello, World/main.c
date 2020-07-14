e353b8c8658e271baa1c1ed021b3e2c024fb342d
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-07-14 12:16:33 -0400

    .

---
+++ Hello, World/Hello, World/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hello, World
+//
+//  Created by Hongjun Yun on 2020-07-14.
+//  Copyright © 2020 Hongjun Yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
