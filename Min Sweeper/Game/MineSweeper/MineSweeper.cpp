42b025548a2be083ff1947734faad7dc7c2bdf8a
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-10-29 01:16:43 -0400

    .

--- Min Sweeper/Game/MineSweeper/MineSweeper.cpp
+++ Min Sweeper/Game/MineSweeper/MineSweeper.cpp
@@ -1,10 +1,20 @@
-﻿// MineSweeper.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <Windows.h>
+#include <mmsystem.h>
+#include <process.h>
 
-int main() {
+#pragma comment(lib, "winmm.lib")
+#pragma warning(disable : 4996)
 
+void moveCurser(short x, short y) {
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int main(void) {
+	char tmp;
+	system("title Mine Sweeper");
+	PlaySound(TEXT("Music.wav"), NULL, SND_ASYNC | SND_LOOP);
+	while (scanf("%c",&tmp) != EOF);
 	return 0;
 }
