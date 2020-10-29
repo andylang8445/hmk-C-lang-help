ed6a8c0468dca78c586db363f4e72bc7244b56e7
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-10-28 20:45:37 -0400

    .

---
+++ MineSweeper/MineSweeper/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MineSweeper
+//
+//  Created by Hongjun Yun on 2020-10-28.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
